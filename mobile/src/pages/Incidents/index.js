a75a6987630f44c30e0dd7d9672ee3bafc9c1bec
ramon141 <ramonbarbosa123456789@gmail.com>
2021-11-19 10:36:29 -0300

    Update 2

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+export default function Incidents() {
+
+
+
+  return (
+    <View />
+
+
+  );
+}
